031dae9f23caa94aca3057af16a5d0e2ee60206f
PHUPhan1707 <phanan.phu17@gmail.com>
2024-03-01 19:41:30 +0700

    Create a board-game for player and computer Build fighting arena for 2 teams Change code for function flip() to rotate 360 deg

---
+++ app.js
@@ -0,0 +1,51 @@
+
+const gamesBoardContainer= document.querySelector('#gamesboard-container')
+const optionContainer=document.querySelector('.option-container')
+const flipButton= document.querySelector('#flip-button')
+
+
+let angle =0;
+function flip() {
+    const optionShips=Array.from(optionContainer.children)
+
+    angle =angle === 0?90 :0
+    optionShips.forEach(optionShip=>optionShip.style.transform=`rotate(${angle}deg)`)
+}
+
+
+flipButton.addEventListener('click',flip)
+
+const width=10
+
+function createBoard(color,user){
+    const gameBoardContainer=document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor= color
+    gameBoardContainer.id=user
+    for (let i=0; i<width*width;i++){
+        const block=document.createElement('div')
+        block.classList.add('block')
+        block.id=i
+        gameBoardContainer.append(block)
+    }
+    gamesBoardContainer.append(gameBoardContainer)
+}
+createBoard('red','player')
+createBoard('pink','computer')
+
+//Creating a ship
+
+class Ship{
+    constructor(name,length) {
+        this.name=name
+        this.length=length
+    }
+
+}
+const destroyer=new Ship('destroyer',2)
+const submarine= new Ship('submarine',3);
+const cruiser =new  Ship('cruiser',3);
+const battleship   = new    Ship ('battleship',4)
+const carrier=new Ship('carrier',5)
+
+const ships=[destroyer,submarine,cruiser,battleship,carrier]
