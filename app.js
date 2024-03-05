b16b41b15a083e66902624836d66166545fc8d1f
PHUPhan1707 <phanan.phu17@gmail.com>
2024-03-05 09:21:18 +0700

    Code add hightLight ( Animation when move the ship)

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const gamesBoardContainer = document.querySelector('#gamesboard-container');
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
+const startButton= document.querySelector('#start-button')
+const infoDisplay=document.querySelector('#info')
+const turnDisplay=document.querySelector('#turn-display')
+
+
 
 let angle = 0;
 
@@ -49,12 +54,7 @@ const ships = [destroyer, submarine, cruiser, battleship, carrier];
 
 let notDropped = false;
 
-function addShipPiece(user, ship, startId) {
-    const allBoardBlocks = document.querySelectorAll(`#${user} div`);
-    let randomBoolean = Math.random() < 0.5;
-    let isHorizontal = user === 'player' ? angle === 0 : randomBoolean;
-    let randomStartIndex = Math.floor(Math.random() * width * width);
-    let startIndex = startId ? startId : randomStartIndex;
+function getValidity(allBoardBlocks,isHorizontal,startIndex,ship){
     let validStart = isHorizontal ? (startIndex <= width * width - ship.length ? startIndex : width * width - ship.length) :
         (startIndex <= width * width - width * ship.length ? startIndex : startIndex - ship.length * width + width);
 
@@ -78,13 +78,28 @@ function addShipPiece(user, ship, startId) {
 
     const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'));
 
+    return{
+        shipBlocks,valid,notTaken
+    }
+}
+
+function addShipPiece(user, ship, startId) {
+    const allBoardBlocks = document.querySelectorAll(`#${user} div`);
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal = user === 'player' ? angle === 0 : randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+
+    let startIndex = startId ? startId : randomStartIndex;
+
+    const { shipBlocks,valid,notTaken}=getValidity(allBoardBlocks,isHorizontal,startIndex,ship)
+
     if (valid && notTaken) {
         shipBlocks.forEach(shipBlock => {
             shipBlock.classList.add(ship.name.toLowerCase());
             shipBlock.classList.add('taken');
         });
     } else {
-        if (user === 'computer') addShipPiece(user, ship); // Corrected recursion by passing 'user' argument
+        if (user === 'computer') addShipPiece(user, ship,startId); // Corrected recursion by passing 'user' argument
         if (user === 'player') notDropped = true;
     }
 }
@@ -109,6 +124,8 @@ function dragStart(e) {
 
 function dragOver(e) {
     e.preventDefault();
+    const ship= [draggedShip.id]
+    highlightArea(e.target.id,ship)
 }
 
 function dropShip(e) {
@@ -119,3 +136,42 @@ function dropShip(e) {
         draggedShip.remove();
     }
 }
+
+// Add highlight
+
+function highlightArea(startIndex,ship)
+{
+    const allBoardBlocks= document.querySelectorAll('#player div')
+    let isHoriztonal= angle===0
+
+    const {shipBlocks,valid,notTaken}=getValidity(allBoardBlocks,isHoriztonal,startIndex,ship)
+
+    if(valid && notTaken){
+        shipBlocks.forEach(shipBlock=>{
+            shipBlock.classList.add('hover')
+            setTimeout(()=> shipBlock.classList.remove('hover'),500)
+        })
+    }
+}
+
+let gameOver = false
+let playerTurn
+function startGame(){
+
+    if(optionContainer.children.length !=0 ){
+        infoDisplay.textContent='Please place all your ship first!'
+    }else {
+        const allBoardBlocks= document.querySelectorAll('#computer div')
+        allBoardBlocks.forEach(block=> block.addEventListener('click',handleClick))
+    }
+}
+
+function handleClick(e){
+    if(!gameOver){
+        if ( e.target.classList.contains('taken')){
+            e.target.classList.add('boom')
+            infoDisplay.textContent='You hit the computer ship'
+        }
+    }
+}
+startButton.addEventListener('click',startGame)
